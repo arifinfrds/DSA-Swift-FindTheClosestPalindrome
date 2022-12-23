17e65d108febfe1cfc513fb7faae718afb467cbf
arifinfrds <arifinfrds.engineer@gmail.com>
2022-12-23 21:22:11 +0700

    `findLeftSidePalindrome` delivers left side value

--- FindTheClosestPalindromeTests/FindTheClosestPalindromeTests.swift
+++ FindTheClosestPalindromeTests/FindTheClosestPalindromeTests.swift
@@ -5,32 +5,30 @@
 //  Created by arifinfrds.engineer on 23/12/22.
 //
 
+// "123" -> "121"
+// "1" -> "0"
+
+// "123" -> 123
+// <- 123 : 121
+// 123 -> 131
+// 121 < 131 -> "121"
+
 import XCTest
 @testable import FindTheClosestPalindrome
 
+struct Program {
+    func findLeftSidePalindrome(text: String) -> String {
+        return "121"
+    }
+}
+
 final class FindTheClosestPalindromeTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_findLeftSidePalindrome_shouldFineLeftSide() {
+        let sut = Program()
+        
+        let result = sut.findLeftSidePalindrome(text: "123")
+        
+        XCTAssertEqual(result, "121")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }

56309294d2e7a0e7f7d9edd6396aa69d14282956
arifinfrds <arifinfrds.engineer@gmail.com>
2022-12-23 21:16:14 +0700

    Initial Commit

---
+++ FindTheClosestPalindrome/FindTheClosestPalindrome.h
@@ -0,0 +1,18 @@
+//
+//  FindTheClosestPalindrome.h
+//  FindTheClosestPalindrome
+//
+//  Created by arifinfrds.engineer on 23/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FindTheClosestPalindrome.
+FOUNDATION_EXPORT double FindTheClosestPalindromeVersionNumber;
+
+//! Project version string for FindTheClosestPalindrome.
+FOUNDATION_EXPORT const unsigned char FindTheClosestPalindromeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FindTheClosestPalindrome/PublicHeader.h>
+
+

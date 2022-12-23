db1d40b0bfa73ef20c3af48e0248d7bfe4d2b3cb
arifinfrds <arifinfrds.engineer@gmail.com>
2022-12-23 21:29:16 +0700

    `findLeftSidePalindrome` can find the left side palindrome

--- FindTheClosestPalindromeTests/FindTheClosestPalindromeTests.swift
+++ FindTheClosestPalindromeTests/FindTheClosestPalindromeTests.swift
@@ -17,8 +17,25 @@ import XCTest
 @testable import FindTheClosestPalindrome
 
 struct Program {
-    func findLeftSidePalindrome(text: String) -> String {
-        return "121"
+    func findLeftSidePalindrome(number: Int) -> Int {
+        
+        var result: Int? = nil
+        var currentNumber = number
+        
+        while result == nil {
+            if currentNumber.isPalindrome && currentNumber != number {
+                result = currentNumber
+            }
+            currentNumber -= 1
+        }
+        
+        return result!
+    }
+}
+
+private extension Int {
+    var isPalindrome: Bool {
+        return String(self) == String(String(self).reversed())
     }
 }
 
@@ -27,8 +44,16 @@ final class FindTheClosestPalindromeTests: XCTestCase {
     func test_findLeftSidePalindrome_shouldFindLeftSide() {
         let sut = Program()
         
-        let result = sut.findLeftSidePalindrome(text: "123")
+        let result = sut.findLeftSidePalindrome(number: 123)
         
-        XCTAssertEqual(result, "121")
+        XCTAssertEqual(result, 121)
+    }
+    
+    func test_findLeftSidePalindrome_shouldFindLeftSideOnSingleDigit() {
+        let sut = Program()
+        
+        let result = sut.findLeftSidePalindrome(number: 1)
+        
+        XCTAssertEqual(result, 0)
     }
 }
